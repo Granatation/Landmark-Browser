71a4a8102b302f6346adc38766307c3bb721a43e
Nikola Emilov <nikico248@gmail.com>
2022-08-10 15:57:40 +0300

    Search bar added to my-profile and all-landmark pages

--- client/src/components/Landmarks/AllLandmarks/AllLandmarksPaged.js
+++ client/src/components/Landmarks/AllLandmarks/AllLandmarksPaged.js
@@ -3,13 +3,17 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import * as landmarkService from '../../../services/landmarkService';
 import { LandmarkContext } from "../../../contexts/LandmarkContext";
+import { AuthContext } from '../../../contexts/AuthContext'
 import { SingleLandmark } from "../SingleLandmark/SingleLandmark";
 import { Error } from "../../Error/Error";
 
 export const AllLandmarksPaged = () => {
     const { landmarks, setLandmarks } = useContext(LandmarkContext);
+    const [filteredLandmarks, setFilteredLandmarks] = useState();
+    const { isAuth } = useContext(AuthContext);
     const [pages, setPages] = useState(0);
     const [serverError, setServerError] = useState('');
+    const [searchString, setSearchString] = useState('');
     const { pageNumber } = useParams();
     const navigate = useNavigate();
 
@@ -18,13 +22,22 @@ export const AllLandmarksPaged = () => {
             .then(result => {
                 if (!result.message) {
                     setLandmarks(result);
+                    setFilteredLandmarks(result);
                     setPages(Math.ceil(result.length / 6));
                 } else throw result
             })
             .catch(error => setServerError(error.message))
     }, []);
 
-    const currentPageLandmarks = landmarks?.slice((0 + (Number(pageNumber) - 1) * 6), (6 + (Number(pageNumber) - 1) * 6));
+    useEffect(() => {
+        if (searchString === '') {
+            setPages(Math.ceil(landmarks.length / 6));
+        } else {
+            setPages(Math.ceil(searchString.length / 6));
+        }
+    }, [searchString])
+
+    const currentPageLandmarks = filteredLandmarks?.slice((0 + (Number(pageNumber) - 1) * 6), (6 + (Number(pageNumber) - 1) * 6));
 
     let liItems = [];
 
@@ -37,6 +50,16 @@ export const AllLandmarksPaged = () => {
             >
                 {i}
             </Link>);
+    }
+
+    const changeHandler = (e) => {
+        setSearchString(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setFilteredLandmarks(landmarks
+            .filter(x => x.name.toLowerCase().includes(searchString.toLowerCase())));
     }
 
     const previousButtonHandler = () => {
@@ -55,10 +78,29 @@ export const AllLandmarksPaged = () => {
         <section id="all-landmarks-page">
             <h1>All Landmarks</h1>
 
+            {
+                isAuth &&
+                <>
+                    <input
+                        id="search-bar"
+                        onChange={changeHandler}
+                        placeholder="Search"
+                        value={searchString}
+                    />
+                    <button
+                        id="search-button"
+                        onClick={submitHandler}
+                        type="submit"
+                    >
+                        Search
+                    </button>
+                </>
+            }
+
             <Error message={serverError} />
 
             <div id="all-landmarks-container">
-                {currentPageLandmarks.length > 0
+                {currentPageLandmarks?.length > 0
                     ? currentPageLandmarks.map(x => <SingleLandmark key={x._id} landmark={x} />)
                     : <h3 className="no-articles">No landmarks yet</h3>
                 }
--- client/src/components/MyProfile/MyProfilePaged.js
+++ client/src/components/MyProfile/MyProfilePaged.js
@@ -11,6 +11,8 @@ export const MyProfilePaged = () => {
     const { landmarks, setLandmarks } = useContext(LandmarkContext);
     const { user, isAuth } = useContext(AuthContext);
     const [serverError, setServerError] = useState('');
+    const [filteredLandmarks, setFilteredLandmarks] = useState()
+    const [searchString, setSearchString] = useState('');
     const [pages, setPages] = useState(0);
     const { pageNumber } = useParams();
     const navigate = useNavigate();
@@ -24,13 +26,22 @@ export const MyProfilePaged = () => {
             .then(result => {
                 if(!result.message){
                     setLandmarks(result);
+                    setFilteredLandmarks(result);
                     setPages(Math.ceil(result.length / 6))
                 }else throw result         
             })
             .catch(error => setServerError(error.message))
     }, []);
 
-    const currentPageLandmarks = landmarks.slice((0 + (Number(pageNumber) - 1) * 6), (6 + (Number(pageNumber) - 1) * 6))
+    useEffect(() => {
+        if (searchString === '') {
+            setPages(Math.ceil(landmarks.length / 6));
+        } else {
+            setPages(Math.ceil(searchString.length / 6));
+        }
+    }, [searchString])
+
+    const currentPageLandmarks = filteredLandmarks?.slice((0 + (Number(pageNumber) - 1) * 6), (6 + (Number(pageNumber) - 1) * 6))
 
     let liItems = []
 
@@ -43,6 +54,16 @@ export const MyProfilePaged = () => {
             >
                 {i}
             </Link>);
+    }
+
+    const changeHandler = (e) => {
+        setSearchString(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setFilteredLandmarks(landmarks
+            .filter(x => x.name.toLowerCase().includes(searchString.toLowerCase())));
     }
 
     const previousButtonHandler = () => {
@@ -64,10 +85,29 @@ export const MyProfilePaged = () => {
                 <h1>{user.username}'s posts</h1>
             </div>
 
+            {
+                isAuth &&
+                <>
+                    <input
+                        id="search-bar"
+                        onChange={changeHandler}
+                        placeholder="Search"
+                        value={searchString}
+                    />
+                    <button
+                        id="search-button"
+                        onClick={submitHandler}
+                        type="submit"
+                    >
+                        Search
+                    </button>
+                </>
+            }
+
             <Error message={serverError} />
 
             <div id="all-landmarks-container">
-                {currentPageLandmarks.length > 0
+                {currentPageLandmarks?.length > 0
                     ? currentPageLandmarks.map(x => <SingleLandmark key={x._id} landmark={x} />)
                     : <h3 className="no-articles">No landmarks yet</h3>
                 }
